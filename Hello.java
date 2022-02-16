ae6667d80d049eab9db510f839bf44bd2b2061b1
josekx1992 <98968172+josekx1992@users.noreply.github.com>
2022-02-16 10:28:56 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("Hello Agni");
+    System.out.println("Hello JP");
 
     System.out.println("Hello Jose");
  }
